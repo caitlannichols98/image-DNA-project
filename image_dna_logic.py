ec1acf67eb1b21fb064edf83cc3d1b211d6384b8
caitlannichols98 <caitlannichols9@gmail.com>
2025-06-05 03:55:29 -0400

    Update image_dna_logic.py

--- image_dna_logic.py
+++ image_dna_logic.py
@@ -26,7 +26,9 @@ def init_db():
 # process image
 def process_image(filepath):
     img = Image.open(filepath).convert("L")  # grayscale
-    img = img.resize((RESIZED_DIM, RESIZED_DIM), Image.ANTIALIAS)
+    # i will be keepin gthis here incase, until i am done with the project. might need old constants. img = img.resize((RESIZED_DIM, RESIZED_DIM), Image.ANTIALIAS)
+    from PIL import Image
+    img = img.resize((RESIZED_DIM, RESIZED_DIM), Image.Resampling.LANCZOS)
     return np.array(img)
 
 # split into grid blocks
